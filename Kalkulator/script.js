233e33c47860e33e4f55af0641425a8935f21460
Hanegalen <tobias.kinn00@gmail.com>
2022-10-12 14:29:19 +0200

    Kalkulator ikke startet

---
+++ Kalkulator/script.js
@@ -0,0 +1,8 @@
+let tall_1 = document.getElementById(input1).value;
+let tall_2 = document.getElementById(input2).value;
+
+function test() {
+    let sum = tall_1 + tall_2;
+    document.getElementById('output').innerHTML = sum;
+}
+
