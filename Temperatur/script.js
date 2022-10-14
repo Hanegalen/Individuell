9be318ca94aab4166aeafafb196c35bf24ac0292
Hanegalen <tobias.kinn00@gmail.com>
2022-10-14 10:23:20 +0200

    x

--- Temperatur/script.js
+++ Temperatur/script.js
@@ -8,8 +8,8 @@ var kelvin;
 function resultat() {
     mV = document.getElementById("input").value;
     celsius = (mV - 500) / 10;
-    fahrenheit = mV * 1.8 + 32;
-    kelvin = mV + 273.15;
+    fahrenheit = celsius * 1.8 + 32;
+    kelvin = celsius + 273.15;
 }
 
 
@@ -22,29 +22,35 @@ function log() {
     var fLengde = fahrenheit.length;
     var cLengde = celsius.length;
 
-    for (celsius[cLengde - 1] == 0 || celsius[cLengde - 1] == "."; cLengde--;) {
-        celsius = (celsius.substring(0, celsius.length -1));
-            if (celsius[cLengde -1] != ".") {
-                cLengde--;
-            }
+    
+    while (celsius[celsius.length - 1] == 0) {
+        celsius = (celsius.substring(0, celsius.length - 1))
+        celsius.length--;
+        if (celsius[celsius.length - 1] == "." && celsius[celsius.length - 1] == ".") {
+            
+        }
+
     }
 
-    for (fahrenheit[fLengde - 1] == 0 || fahrenheit[fLengde - 1] == "."; fLengde--;) {
+/*    while (celsius[cLengde - 1] == 0 || celsius[cLengde - 1] == "." && celsius[cLengde - 2] + celsius[cLengde - 1] != Number + ".") {
+        celsius = (celsius.substring(0, celsius.length -1));
+        cLengde--;
+    }
+
+    while (fahrenheit[fLengde - 1] == 0 || fahrenheit[fLengde - 1] == "." && fahrenheit[fLengde - 2] + fahrenheit[fLengde - 1] != Number + ".") {
         fahrenheit = (fahrenheit.substring(0, fahrenheit.length -1));
-            if (fahrenheit[fLengde -1] != ".") {
-                fLengde--;
-            }
+        fLengde--;
     }
-    for (kelvin[kLengde - 1] == 0 || kelvin[kLengde - 1] == "."; kLengde--;) {
+
+    while (kelvin[kLengde - 1] == 0 || kelvin[kLengde - 1] == "." && kelvin[kLengde] - 2 + kelvin[kLengde - 1] != Number + ".") {
         kelvin = (kelvin.substring(0, kelvin.length -1));
-            if (kelvin[kLengde -1] != ".") {
-                kLengde--;
-            }
+        kLengde--;
     }
-    
-    console.log(celsius);
-    console.log(fahrenheit);
-    console.log(kelvin);
+*/
+
+    console.log(celsius + " Celsius");
+    console.log(fahrenheit + " Fahrenheit");
+    console.log(kelvin + " Kelvin");
 
 }
 
